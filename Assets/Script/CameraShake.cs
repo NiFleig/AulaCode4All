45f99b4d29738c9b90b66127a2d84a125d5bc6f9
DESKTOP-585MQBD\Claudio <nicolmfleig@gmail.com>
2021-08-07 16:10:09 -0300

    cameras, event powerup inicio

---
+++ Assets/Script/CameraShake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float duration;
+    public float force;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public void Shake()
+    {
+      Vector3 currentPosition = transform.localPosition;
+
+      float elapsedTime = 0f;
+
+      while(elapsedTime < duration)
+      {
+        float shakeX = Random.Range(minX, maxX)*force;
+        float shakeY = Random.Range(minY, maxY)*force;
+
+        transform.localPosition = new Vector3(shakeX, shakeY, currentPosition.z);
+
+        elapsedTime += Time.deltaTime;
+
+        Debug.Log("shaking");
+      }
+
+      transform.localPosition = currentPosition;
+      Debug.Log("Stop Shaking");
+    }
+}
