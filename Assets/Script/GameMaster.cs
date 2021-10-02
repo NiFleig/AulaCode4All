7bac9fdc630e908ba93801d4e682138fd58852e5
rodigo <nicolmfleig@gmail.com>
2021-10-02 18:40:40 -0300

    reset

--- Assets/Script/GameMaster.cs
+++ Assets/Script/GameMaster.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -39,42 +38,3 @@ public class GameMaster : MonoBehaviour
         playerPrefab.transform.position = spawnPosition;
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameMaster : MonoBehaviour
-{
-    public static GameMaster gm;
-
-    public Transform playerPrefab;
-    public Transform spawnPoint;
-
-    public List<Transform> checkpoints = new List<Transform>();
-
-    public Vector3 spawnPosition {get {return checkpoints[checkpoints.Count -1].position; } }
-    public CheckPoint curCheckpoint {get { return checkpoints[checkpoints.Count - 1].GetComponent<CheckPoint>();}}
-
-    public float spawnPointReset = 0;
-
-    void Awake()
-    {
-        if(gm == null)
-        {
-            gm = this;
-            checkpoints.Add(spawnPoint);
-        }
-    }
-    
-    public static void KillPlayer(PlayerState player)
-    {
-        gm.RespawnPlayer();
-    }
-
-    public void RespawnPlayer()
-    {
-        playerPrefab.transform.position = spawnPosition;
-    }
-}
->>>>>>> main
