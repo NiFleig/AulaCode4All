24e646b3131d33d547e4e552c0830dcc036dd931
rodigo <nicolmfleig@gmail.com>
2021-10-16 19:04:30 -0300

    Frog

---
+++ Assets/Script/LineControler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineControler : MonoBehaviour
+{
+
+    public LineRenderer lr;
+    public Transform child;
+
+    void Start()
+    {
+        lr.SetPosition(0,transform.position);
+    }
+
+    void Update()
+    {
+        lr.SetPosition(1, child.position);
+    }
+}
---
+++ Assets/Script/Swing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+    public float range = 20f;
+
+    public LayerMask layer;
+
+    public GameObject swing;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, AimDirection(), range, layer);
+
+            if(hit.collider != null)
+            {
+                print(hit.collider.name);
+                InstantiateSwingObject(hit.point);
+            }
+        }
+        Debug.DrawRay(transform.position, AimDirection(), Color.red);
+    }
+
+    void InstantiateSwingObject(Vector2 position)
+    {
+        Instantiate(swing, position, Quaternion.identity);
+    }
+
+    Vector3 AimDirection()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return (mousePos - transform.position).normalized;
+    }
+}
