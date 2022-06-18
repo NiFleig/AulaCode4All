6e5fcd48100a02609b239a15feeb0a2ee8a739c8
Rodrigo Fleig <nicolmfleig@gmail.com>
2022-06-18 19:43:43 -0300

    Chicken Glide

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -78,80 +78,8 @@ public class Player : MonoBehaviour
         }
 
         if(currentState != states.death)
-        {
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                if(controller2D.info.down)
-                {
-                    velocity.y = vSpeed;
-                    jump.Play();
-                }else if(!controller2D.info.down && doubleJump.jump && !doubleJump.djump)
-                {
-                    velocity.y = vSpeed;
-                    doubleJump.djump = true;
-                    jump.Play();
-
-                    playerAnim.SetBool("DoubleJump", true);
-                }
-
-                if(wallJump.wSlide)
-                {
-                    if(input.x == 0)
-                    {
-                        velocity.x = -wallJump.wDirection * wallJump.wJump.x;
-                        velocity.y = wallJump.wJump.y;
-                        jump.Play();
-                        playerAnim.SetBool("WallJump", true);
-                    }else if(input.x == wallJump.wDirection)
-                    {
-                        velocity.x = -wallJump.wDirection * wallJump.wHop.x;
-                        velocity.y = wallJump.wHop.y;
-                        jump.Play();
-                        playerAnim.SetBool("WallJump", true);
-                    }else
-                    {
-                        velocity.x = -wallJump.wDirection * wallJump.wLeap.x;
-                        velocity.y = wallJump.wLeap.y;
-                        jump.Play();
-                        playerAnim.SetBool("WallJump", true);
-                    }
-                }
-
-                //TODO
-                if (swing.isHooked && controller2D.info.down)
-                {
-                    swing.distanceJoint2D.distance -= 7;
-                }
-            }
-
-            if(Input.GetKey(KeyCode.Space) && doubleJump.djump == true)
-            {
-                glideFactor = .1f;
-            }else
-            {
-                glideFactor = 1f;
-            }
-
-            if(Input.GetKeyDown(KeyCode.RightShift) && currentState == Player.states.bear)
-            {
-                attack.Action();
-
-                playerAnim.SetTrigger("Attacking");
-            }
-
-            if(Input.GetKey(KeyCode.RightShift) && wallClimb.wClimb)
-            {
-                velocity = new Vector2(velocity.x, 0);
-
-                float wSpeed = input.y > 0 ? wallClimb.wClimbSpeed : 1;
-
-                velocity = new Vector2(velocity.x, input.y * (hSpeed * wSpeed));
-
-                playerAnim.SetBool("Climbing", true);
-            }else
-            {
-                playerAnim.SetBool("Climbing", false);
-            }
+        {  
+            DetectInput();
 
             if(swing.isHooked == false)
             {
@@ -191,7 +119,61 @@ public class Player : MonoBehaviour
             }
             input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-            if(Input.GetKeyDown(KeyCode.Space))
+
+            float targetVeloX = input.x * hSpeed;
+
+            velocity.x = Mathf.SmoothDamp(velocity.x, targetVeloX, ref smoothVelocity, (controller2D.info.down) ? groundAcceleration : airAcceleration);
+            print($"Vy: {velocity.y} | g: {grav}");
+            if(currentState == states.chicken)
+            {
+                if(velocity.y < 0)
+                {
+                    velocity.y += grav * glideFactor * Time.deltaTime;
+                }else
+                {
+                    velocity.y += grav * Time.deltaTime;
+                }
+            }else
+            {
+                velocity.y += grav * Time.deltaTime;
+            }
+
+            if (!swing.isHooked)
+                controller2D.Move(velocity * Time.deltaTime);
+            else
+            {
+                if(swing.swingPosition != null)
+                {
+                    //if (!controller2D.info.down)
+                    if (swing.ShouldSwing)
+                    {
+                        //Movimento de balan�o
+                        if (Vector2.Distance(transform.position, swing.swingPosition.position) > .25f)
+                        {
+                            var direction = (swing.swingPosition.position - transform.position).normalized;
+                            controller2D.Move(direction * swing.SwingSpeed * Time.deltaTime, false);
+
+                            transform.position = Vector3.Lerp(transform.position, swing.swingPosition.position + SwingSpotOffset, swing.SwingSpeed * Time.deltaTime);
+                        }
+                    }
+                    else
+                    {
+                        //Movimento de andar (normal)
+                        swing.distanceJoint2D.distance = Vector3.Distance(transform.position, swing.currentSwingObject.transform.position);
+                        controller2D.Move(velocity * Time.deltaTime);
+                        swing.swingPosition.transform.position = transform.position - SwingSpotOffset;
+                        swing.swingPosition.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    }
+                } 
+            }
+
+            
+        }
+    }
+
+    void DetectInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
             {
                 if(controller2D.info.down)
                 {
@@ -229,11 +211,18 @@ public class Player : MonoBehaviour
                     }
                 }
 
-                //TODO
                 if (swing.isHooked && controller2D.info.down)
                 {
                     swing.distanceJoint2D.distance -= 7;
                 }
+            }
+
+            if(Input.GetKey(KeyCode.Space) && doubleJump.djump == true)
+            {
+                glideFactor = .1f;
+            }else
+            {
+                glideFactor = 1f;
             }
 
             if(Input.GetKeyDown(KeyCode.RightShift) && currentState == Player.states.bear)
@@ -257,73 +246,10 @@ public class Player : MonoBehaviour
                 playerAnim.SetBool("Climbing", false);
             }
 
-            /*
-            if(Input.GetKeyDown(KeyCode.Alpha1) && playerState.bearEquip == true)
-            {
-                currentState = states.bear;
-                playerAnim = animatorList[1];
-
-                wallJump.wSlide = false;
-            }
-            if(Input.GetKeyDown(KeyCode.Alpha2) && playerState.chickenEquip == true)
-            {
-                currentState = states.chicken;
-                playerAnim = animatorList[2];
-
-                wallJump.wSlide = false;
-            }
-            if(Input.GetKeyDown(KeyCode.Alpha3) && playerState.monkeyEquip == true)
-            {
-                currentState = states.monkey;
-                playerAnim = animatorList[3];
-            }
-            if(Input.GetKeyDown(KeyCode.Alpha4) && playerState.fishEquip == true)
-            {
-                currentState = states.fish;
-                playerAnim = animatorList[4];
-
-                wallJump.wSlide = false;
-            }
-            */
-            float targetVeloX = input.x * hSpeed;
-
-            velocity.x = Mathf.SmoothDamp(velocity.x, targetVeloX, ref smoothVelocity, (controller2D.info.down) ? groundAcceleration : airAcceleration);
-            velocity.y += grav * glideFactor * Time.deltaTime;
-
-            if (!swing.isHooked)
-                controller2D.Move(velocity * Time.deltaTime);
-            else
-            {
-                if(swing.swingPosition != null)
-                {
-                    //if (!controller2D.info.down)
-                    if (swing.ShouldSwing)
-                    {
-                        //Movimento de balan�o
-                        if (Vector2.Distance(transform.position, swing.swingPosition.position) > .25f)
-                        {
-                            var direction = (swing.swingPosition.position - transform.position).normalized;
-                            controller2D.Move(direction * swing.SwingSpeed * Time.deltaTime, false);
-
-                            transform.position = Vector3.Lerp(transform.position, swing.swingPosition.position + SwingSpotOffset, swing.SwingSpeed * Time.deltaTime);
-                        }
-                    }
-                    else
-                    {
-                        //Movimento de andar (normal)
-                        swing.distanceJoint2D.distance = Vector3.Distance(transform.position, swing.currentSwingObject.transform.position);
-                        controller2D.Move(velocity * Time.deltaTime);
-                        swing.swingPosition.transform.position = transform.position - SwingSpotOffset;
-                        swing.swingPosition.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                    }
-                } 
-            }
-
             if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
             {
                 pause = !pause;
             }
-        }
     }
 
 }
